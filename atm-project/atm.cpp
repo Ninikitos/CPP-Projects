570d91bd0ba1020ca4fd6755658f15684a9cea45
Nick Ilin <ninikitos90@gmail.com>
2022-08-11 04:08:56 -0400

    handeling invalid pin inputs

--- atm-project/atm.cpp
+++ atm-project/atm.cpp
@@ -1,72 +1,144 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
-bool check_pin (int input, int pin);
-void print_menu ();
-void balance_check (int balance);
+using namespace std;
+
+void check_pin(int input, int pin);
+void print_menu();
+void balance_check(float balance);
+float deposite(float balance);
 
 int main ()
 {
+    bool done = false;
     int pin = 4311;
-    bool is_correct = false;
     int input = 0;
-    int option = 0;
-    int balance = 10000;
+    unsigned option = 0;
+    float balance = 10000;
 
-    do
+    while (input != pin)
     {
-        is_correct = check_pin(input, pin);
-        
-    } while (!is_correct);
-
-    print_menu();
-    std::cin >> option;
-
-    switch (option)
+        cout << "Enter your pin: " << endl;
+        cin >> input;
+        check_pin(input, pin);
+        if (cin.fail())
+        {
+            cout << "|--------------------------|" << endl;
+            cout << "| Pin must be only digits. |" << endl;
+            cout << "|--------------------------|" << endl;
+            cin.clear();
+            cin.ignore(10, '\n');
+        }
+    }
+    
+    while(!done)
     {
-    case 1:
-        balance_check(balance);
-        break;    
-    default:
-        break;
+        print_menu();
+        cin >> option;
+        cout << endl;
+
+        switch (option)
+        {
+        case 1:
+            balance_check(balance);
+            break; 
+        case 2:
+            balance = deposite(balance);
+            break;
+        case 0:
+            done = true;
+        default:
+
+            break;
+        }
     }
 }
 
-bool check_pin (int input, int pin)
+void check_pin(int input, int pin)
 {
-    std::cout << "Enter your pin: ";
-    std::cin >> input;
-
     if (input == pin)
     {
-        std::cout << "***** Login successful. *****" << std::endl;
-        std::cout << std::endl;
-        std::cout << "***** Welcome to Bank of America. *****" << std::endl;
-        std::cout << std::endl;
-
-        return true;
+        cout << "***** Login successful. *****" << endl;
+        cout << endl;
+        cout << "***** Welcome to Bank of America. *****" << endl;
+        cout << endl;
+        return;
     }
-    else 
+
+    int counter = 0;
+    do
     {
-        std::cout << "Pin is incorrect." << std::endl;
-        return false;
+        input = input / 10;
+        counter++;
+    } while (input >= 10);
+
+    if (isdigit(input) == 0 && input != 0)
+    {
+        if (counter < 3)
+        {
+            cout << "|------------------|" << endl;
+            cout << "| Pin is too short.|" << endl;
+            cout << "|------------------|" << endl;
+            cout << endl;
+        }
+        else if (counter >= 4)
+        {
+            cout << "|-----------------|" << endl;
+            cout << "| Pin is too long.|" << endl;
+            cout << "|-----------------|" << endl;
+            cout << endl;
+        }
+        else if (counter == 3 && input != pin)
+        {
+            cout << "|------------------|" << endl;
+            cout << "| Pin is incorrect.|" << endl;
+            cout << "|------------------|" << endl;
+            cout << endl;
+            return;
+        } 
+    }
+
+    if (input != pin && isdigit(input) != 0)
+    {   
+        cout << "|------------------|" << endl;
+        cout << "| Pin is incorrect.|" << endl;
+        cout << "|------------------|" << endl;
+        cout << endl;
+        return;
     }
 }
 
-void print_menu ()
+void print_menu()
 {
-    std::cout << "***** Menu *****" << std::endl;
-    std::cout << std::endl;
-    std::cout << "1. Check balance" << std::endl;
-    std::cout << std::endl;
-    std::cout << "2. Deposite money" << std::endl;
-    std::cout << std::endl;
-    std::cout << "3. Withdrawal money" << std::endl;
-    std::cout << std::endl;
-    std::cout << "0. Exit" << std::endl;
-    std::cout << std::endl;
+    cout << "***** Menu *****" << endl;
+    cout << endl;
+    cout << "1. Check balance" << endl;
+    cout << endl;
+    cout << "2. Deposite money" << endl;
+    cout << endl;
+    cout << "3. Withdrawal money" << endl;
+    cout << endl;
+    cout << "0. Exit" << endl;
+    cout << endl;
 }
 
-void balance_check (int balance)
+void balance_check(float balance)
+{   cout << "------------------------------" << endl;
+    cout << "Your current balance is: " << balance << endl;
+    cout << "------------------------------" << endl;
+    cout << endl;
+}
+
+float deposite(float balance)
 {
-    std::cout << "Your current balance is: " << balance << std::endl;
+    cout << "Enter amount to deposite: ";
+    float amount = 0;
+    cin >> amount;
+    balance += amount;
+    cout << endl;
+    cout << "-----------------------------" << endl;
+    cout << "Your new balance is: " << balance << endl;
+    cout << "-----------------------------" << endl;
+    return balance;
 }
